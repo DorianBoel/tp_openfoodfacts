481d2608db676be9aa10ac15878ee89169f03a2f
APdu77 <apdu77@yahoo.fr>
2022-10-25 17:43:31 +0200

    last commit

--- src/main/java/tp_openfoodfacts/enums/NutriScore.java
+++ src/main/java/tp_openfoodfacts/enums/NutriScore.java
@@ -1,12 +1,37 @@
 package tp_openfoodfacts.enums;
 
+
+
+
 public enum NutriScore {
+	
 
-	A,
-	B,
-	C,
-	D,
-	E,
-	F;
+	A ("a"), B("b"), C("c"), D("d"), E("e"), F("f");
 
+	private String libelle;
+
+	private NutriScore(String libelle) {
+		this.libelle = libelle;
+	}
+
+	static NutriScore affichage(String lib) {
+
+		NutriScore[] nutSco = NutriScore.values();
+		for (NutriScore n : nutSco) {
+			if (n.getLibelle().equalsIgnoreCase(lib)) {
+				return n;
+			}
+
+		}
+		return null;
+
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
 }

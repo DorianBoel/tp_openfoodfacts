f7255ad02d0170b55d38ca72e6f034476a08861b
Alexandre Ramier <aramier22@outlook.com>
2022-10-25 14:55:19 +0200

    first commit.

---
+++ src/main/java/tp_openfoodfacts/entites/Allergene.java
@@ -0,0 +1,21 @@
+package tp_openfoodfacts.entites;
+
+public class Allergene {
+	String libelleAllergene;
+
+	public Allergene(String libelleAllergene) {
+		super();
+		this.libelleAllergene = libelleAllergene;
+	}
+
+	public String getLibelleAllergene() {
+		return libelleAllergene;
+	}
+
+	public void setLibelleAllergene(String libelleAllergene) {
+		this.libelleAllergene = libelleAllergene;
+	}
+	
+	
+
+}
